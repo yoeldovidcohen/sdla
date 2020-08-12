c3a3f1b7cdf44cdffde4b5f459c1b9666de3d167
Yoel Dovid Cohen <yoeldovidcohen@gmail.com>
2020-08-12 17:52:56 +0100

    sdl compfermed

--- main.cpp
+++ main.cpp
@@ -14,6 +14,16 @@ int main(int argc, char *args[])
     //The surface contained by the window
     SDL_Surface *screenSurface = NULL;
 
+    SDL_version compiled;
+    SDL_version linked;
+
+    SDL_VERSION(&compiled);
+    SDL_GetVersion(&linked);
+    printf("We compiled against SDL version %d.%d.%d ...\n",
+           compiled.major, compiled.minor, compiled.patch);
+    printf("But we are linking against SDL version %d.%d.%d.\n",
+           linked.major, linked.minor, linked.patch);
+
     //Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
